df1d165449d566493a2b192c137c16634ebc786c
Guilherme Braga <guibragamarinho@gmail.com>
2020-09-29 16:53:45 -0300

    Atividades 3

---
+++ Atividades/Atividade03/atividade3,2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){  
+    int x=1;
+    int y=1;
+    int z;
+    for (int x = 1 ; x <=55; x=x+y)
+    {
+       z=x+y;
+       y=z;
+       
+       
+     
+       printf("%d %d ",  x, z);
+           
+
+    }
+    
+
+}
---
+++ Atividades/Atividade03/atividade3,3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+int fat;
+int n= 5;
+for (int fat =1; n>1; n--)
+{
+    fat*=n;
+    
+    printf("%d\n", fat);
+}
+
+
+
+
+
+
+    
+    
+}
